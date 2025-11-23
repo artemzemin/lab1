510994f156d6b907d57f986508c3f54bda8a01a2
megatemik <103453323+artemzemin@users.noreply.github.com>
2025-11-23 12:08:46 -0800

    Update program lab4;.pas

--- program lab4;.pas
+++ program lab4;.pas
@@ -1,35 +1,45 @@
 program lab4;
-    const
-        NMAX:=50;
     var
-        F:array [1...NMAX] of integer;
-        N,i,k:integer;
-        P,S:real;
+        F:array [1..50] of integer;
+        N,i:integer;
+        P,S,Sr:integer;
     begin  
         begin
-            writeln('Введите размер массива N=',);
+            repeat
+            write('Введите размер массива N=');
             readln(N);
-            for i:=1 to N do
+            until (N >= 1) and (N <= 50);
         end;
 
+        for i:=1 to N do
         begin
             write('F[',i,']=');
             readln(F[i]);
         end;
-//
-            S:=0;
-            for i:=1 to N do
+        S:=0; //сумма нечетных элементов 
+        P:=0; //сумма четных элементов
+        for i:=1 to N do
+        begin
             if F[i] mod 2=0 then// mod это оператор который вычисляет остаток целочисленого деления двух чисел
-            S:=S+F[i];
-        writeln('Сумма четных элементов массива:', S);
-
-    
-    
-    
+                S:=S+F[i]//должен выводить нечетные числа 
+            else
+                P:=P+F[i];//должен выводить четные числа
+        end;
+        //writeln('Сумма четных элементов массива:', S);
+        //writeln('Сумма четных элементов массива:', P);
+        
+        Sr:=P-S;//вычесление разности массива
+        for i:= 1 to N do
+        begin
+            if F[i]>0 then 
+            F[i]:= Sr;
+        end;
+        
+        
         writeln('Преобразованный массив');
         for i:=1 to N do
-        write(F[i]:6);
+        begin
+            write(F[i]:6);
+        end;
         writeln;
     end.
-    
-    
